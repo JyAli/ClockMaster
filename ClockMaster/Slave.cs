69040c247bdba646e756585d017b91ce519364ff
Schrödinger <77663569+JyAli@users.noreply.github.com>
2024-05-15 03:32:57 +0300

    Add project files.

---
+++ ClockMaster/Slave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockMaster
+{
+    public class Slave
+    {
+        public string ID { get; private set; }
+        public string PortName { get; private set; }
+
+        public Slave(string id, string portName)
+        {
+            ID = id;
+            PortName = portName;
+        }
+    }
+}
