4d402b93889166bac7bbe979cfe6c2f7900751f3
Schrödinger <ali.aljubran2001@gmail.com>
2024-05-15 16:46:28 +0300

    First Commit

---
+++ ClockMaster/Clock.cs
@@ -0,0 +1,147 @@
+﻿using ClockMaster;
+using System.IO.Ports;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Timers;
+using Timer = System.Timers.Timer;
+
+internal class Clock
+{
+    private static List<SerialPort> Ports = new List<SerialPort>();
+    private static Timer ScanTimer;
+    private static Timer IDTimer;
+
+    private static List<Slave> Slaves = new List<Slave>();
+    private static List<Task> tasks = new List<Task>();
+
+    private static void Main(string[] args)
+    {
+        InitScanTimer();
+        InitIDTimer();
+        StartReceivingData();
+        Console.ReadLine();
+        //Display all port names connected to the device
+        /*foreach (string portName in SerialPort.GetPortNames())
+        {
+            Console.WriteLine("   {0}", portName);
+        }
+
+
+        Port = new SerialPort("COM3", 115200, Parity.None, 8, StopBits.One);
+        Port.NewLine = Environment.NewLine;
+
+        Port.Open();
+        StartReceivingData();
+
+        string? input = "";
+
+        //Keeps asking user for input and sends the input to the port
+        while (input != "End")
+        {
+            input = Console.ReadLine();
+            Port.WriteLine(input);
+        }
+
+        Task.WhenAll(tasks);
+
+        Port.Close();*/
+    }
+
+    private static void InitScanTimer()
+    {
+        ScanTimer = new Timer();
+        ScanTimer.Elapsed += delegate { ScanPorts(); };
+        ScanTimer.Interval = 5000; // in miliseconds
+        ScanTimer.AutoReset = true;
+        ScanTimer.Start();
+    }
+    private static void InitIDTimer()
+    {
+        IDTimer = new Timer();
+        IDTimer.Elapsed += delegate { RequestIDs(); };
+        IDTimer.Interval = 5000; // in miliseconds
+        IDTimer.AutoReset = true;
+        IDTimer.Start();
+    }
+
+    private static void RequestIDs()
+    {
+        //Filters out the ports that are slaves already
+        SerialPort[] nonSlavePorts = Ports.Where(x => Slaves.All(y => x.PortName != y.Port.PortName)).ToArray();
+        foreach (SerialPort port in nonSlavePorts)
+        {
+            port.WriteLine(Commands.REQUEST_ID_COMMAND);
+        }
+    }
+
+    private static void ScanPorts()
+    {
+        string[] systemPortNames = SerialPort.GetPortNames();
+        SerialPort[] deadPorts = Ports.Where(x => systemPortNames.All(y => x.PortName != y)).ToArray();
+        foreach (SerialPort port in deadPorts)
+        {
+            Ports.Remove(port);
+            Slaves.RemoveAll(x => x.Port.PortName == port.PortName);
+        }
+
+        Console.WriteLine("Scanning...");
+        foreach (string portName in systemPortNames)
+        {
+            Console.WriteLine("   {0}", portName);
+            if (Ports.All(x => x.PortName != portName))
+            {
+                SerialPort newPort = new SerialPort(portName, 115200, Parity.None, 8, StopBits.One);
+                newPort.Open();
+                Ports.Add(newPort);
+            }
+        }
+    }
+
+    private static void StartReceivingData()
+    {
+        tasks.Add(StartListening());
+    }
+
+    private static async Task StartListening()
+    {
+        try
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    foreach (SerialPort port in Ports)
+                    {
+                        if (Slaves.Any(x => x.Port.PortName == port.PortName)) continue;
+
+                        try
+                        {
+                            string message = port.ReadLine();
+                            Console.WriteLine($"{port.PortName}: {message}");
+
+                            if (message.StartsWith("ID:"))
+                            {
+                                string id = message.Split(':')[1].Trim();
+                                Slave newSlave = new Slave(id, port);
+                                newSlave.OnPortDisconnected += NewSlave_OnSlaveDisconnected; ;
+                                Slaves.Add(newSlave);
+                            }
+                        }
+                        catch (TimeoutException)
+                        {
+                            //There will be a timeout/Operation timed out exception going mental here so nothing is really needed here...
+                            Console.WriteLine($"{port.PortName}: Time Out!");
+                        }
+                        catch { /*there's some IO exception here because of the Task.WhenAll(tasks);*/ }
+                    }
+                }
+            });
+        }
+        catch { }
+    }
+
+    private static void NewSlave_OnSlaveDisconnected(object? sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
--- ClockMaster/Slave.cs
+++ ClockMaster/Slave.cs
@@ -1,20 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO.Ports;
+using Timer = System.Timers.Timer;
 
 namespace ClockMaster
 {
     public class Slave
     {
         public string ID { get; private set; }
-        public string PortName { get; private set; }
+        public SerialPort Port { get; private set; }
 
-        public Slave(string id, string portName)
+        private List<Task> tasks = new List<Task>();
+        private Timer StatusTimer;
+
+        public event EventHandler OnPortDisconnected;
+        public Slave(string id, SerialPort port)
         {
             ID = id;
-            PortName = portName;
+            Port = port;
+            InitStatusTimer();
+            StartReceivingData();
+        }
+
+        private void InitStatusTimer()
+        {
+            StatusTimer = new Timer();
+            StatusTimer.Elapsed += delegate { CheckStatus(); };
+            StatusTimer.Interval = 1000; // in miliseconds
+            StatusTimer.AutoReset = true;
+            StatusTimer.Start();
+        }
+
+        private void CheckStatus()
+        {
+            string[] systemPorts = SerialPort.GetPortNames();
+            if (!systemPorts.Any(x => x == Port.PortName))
+            {
+                Port.Close();
+                OnPortDisconnected?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        private void StartReceivingData()
+        {
+            tasks.Add(StartListening());
+        }
+
+        private async Task StartListening()
+        {
+            try
+            {
+                await Task.Factory.StartNew(() =>
+                {
+                    while (Port.IsOpen)
+                    {
+                        try
+                        {
+                            string message = Port.ReadLine();
+
+                            //ReceivedMessages.Items.Add(message);
+                            Console.WriteLine(message);
+                        }
+                        catch (TimeoutException)
+                        {
+                            //There will be a timeout/Operation timed out exception going mental here so nothing is really needed here...
+                            Console.WriteLine("Time Out!");
+                        }
+                        catch { /*there's some IO exception here because of the Task.WhenAll(tasks);*/ }
+                    }
+                });
+            }
+            catch { }
         }
     }
 }
